4aadc99b8c0ce5bdd4e69082eb1e6c8e71cf7d71
David Luecke <daff@neyeon.com>
2015-11-07 12:03:13 -0800

    Upgrading to ES6, common querying and latest plugin infrastructure.

--- README.md
+++ README.md
@@ -1,143 +1,20 @@
-# feathers-memory [![Build Status](https://travis-ci.org/feathersjs/feathers-memory.svg?branch=master)](https://travis-ci.org/feathersjs/feathers-memory)[![Code Climate](https://codeclimate.com/github/feathersjs/feathers-memory.png)](https://codeclimate.com/github/feathersjs/feathers-memory)
+# feathers-memory
 
-> An in memory CRUD service for [feathers](http://feathersjs.com)
+[![Build Status](https://travis-ci.org/feathersjs/feathers-memory.png?branch=master)](https://travis-ci.org/feathersjs/feathers-memory)
 
-## Getting Started
+> An in memory service store
 
-Install the module with: `npm install feathers-memory --save`
+## About
 
-```js
-var feathers = require('feathers');
-var memory = require('feathers-memory')();
 
-app.configure(feathers.rest()).use('/users', memory);
-```
-
-## Documentation
-
-#### API
-
-The feathers-memory service follows the same convention as all the other services. Therefore, it provides the following methods:
-
-`find`, `get`, `create`, `update`, `patch`, `remove` and `setup`.
-
-```js
-var memoryService = {
-  find: function(params, callback) {},
-  get: function(id, params, callback) {},
-  create: function(data, params, callback) {},
-  update: function(id, data, params, callback) {},
-  patch: function(id, data, params, callback) {},
-  remove: function(id, params, callback) {},
-  setup: function(app) {}
-}
-```
-
-#### Usage:
-
-```js
-var feathers = require('feathers');
-var memory = require('feathers-memory')();
-var app = feathers();
-
-app.configure(feathers.rest())
-   .use('/users', memory)
-   .listen(8080);
-```
-
-#### Extending:
-
-You can also extend any of the feathers services to do something custom.
-
-```js
-var feathers = require('feathers');
-var memory = require('feathers-memory')();
-var app = feathers();
-
-var myUserService = memory.extend({
-  find: function(params, cb){
-    // Do something awesome!
-    
-    console.log('I am extending the find method');
-    
-    this._super.apply(this, arguments);
-  }
-});
-
-app.configure(feathers.rest())
-   .use('/users', myUserService)
-   .listen(8080);
-```
-
-#### Advanced Querying
-
-You are probably also going to want to filter your data. You can do that by passing options via the body or in a query string. Like so:
-
-```
-GET /users?name=eric&limit=10&skip=10
-```
-
-__Sort:__
-
-```
-GET /users?sort[]=name&sort[]=age
-```
-
-__Order:__
-
-```
-GET /users?order=ascending
-```
-
-__Skip:__
-
-```
-GET /users?skip=10
-```
-
-__Limit:__
-
-```
-GET /users?limit=10
-```
-
-## Examples
-See [examples directory](https://github.com/feathersjs/feathers-memory/tree/master/examples).
-
-## Contributing
-In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
-
-## Release History
-__0.3.0__
-
-- Fixing how the module is exported.
-- Adding `PATCH` support back in
-
-__0.2.2__
-
-- Minor bug fixes
-
-__0.2.1__
-
-- Now utilizing [feathers-errors](https://github.com/feathersjs/feathers-errors)
-- Proper documentation
-
-__0.2.0__
-
-- Unknown
-
-__0.1.2__
-
-- Unknown
-
-__0.1.1__
-
-- Unknown
+## Changelog
 
 __0.1.0__
 
 - Initial release
 
 ## License
-Copyright (c) 2014 [Eric Kryski](https://github.com/ekryski)
-Licensed under the [MIT license](https://github.com/feathersjs/feathers-memory/blob/master/LICENSE-MIT).
+
+Copyright (c) 2015
+
+Licensed under the [MIT license](LICENSE).
---
+++ src/index.js
@@ -0,0 +1,181 @@
+import _ from 'lodash';
+import Proto from 'uberproto';
+import filter from 'feathers-query-filters';
+import { types as errors } from 'feathers-errors';
+
+export const specialFilters = {
+  $in(key, ins) {
+    return current => ins.indexOf(current[key]) !== -1;
+  },
+
+  $nin(key, nins) {
+    return current => nins.indexOf(current[key]) === -1;
+  },
+
+  $lt(key, value) {
+    return current => current[key] < value;
+  },
+
+  $lte(key, value) {
+    return current => current[key] <= value;
+  },
+
+  $gt(key, value) {
+    return current => current[key] > value;
+  },
+
+  $gte(key, value) {
+    return current => current[key] >= value;
+  },
+
+  $not(key, value) {
+    return current => current[key] !== value;
+  }
+};
+
+function filterSpecials(values, query) {
+  if(query.$or) {
+    values = values.filter(current => {
+      return _.some(query.$or, or => _.isMatch(current, or));
+    });
+    delete query.$or;
+  }
+
+  _.each(query, (value, key) => {
+    if(_.isObject(value)) {
+      _.each(value, (target, prop) => {
+        if(specialFilters[prop]) {
+          values = values.filter(specialFilters[prop](key, target));
+        }
+      });
+
+      delete query[key];
+    }
+  });
+
+  return values;
+}
+
+function sorter($sort) {
+  return (first, second) => {
+    let comparator = 0;
+    _.each($sort, (modifier, key) => {
+      if(first[key] < second[key]) {
+        comparator -= 1 * modifier;
+      }
+
+      if(first[key] > second[key]) {
+        comparator += 1 * modifier;
+      }
+    });
+    return comparator;
+  };
+}
+
+const MemoryService = Proto.extend({
+  init(options) {
+    options = options || {};
+
+    this.type = 'memory';
+    this._id = options.idField || 'id';
+    this._uId = options.startId || 0;
+    this.store = options.store || {};
+  },
+
+  find(params, cb) {
+    const query = params.query || {};
+    const filters = filter(query);
+
+    let values = filterSpecials(_.values(this.store), query);
+
+    if(!_.isEmpty(query)) {
+      values = _.where(values, query);
+    }
+
+		// Handle $sort
+		if (filters.$sort) {
+      values.sort(sorter(filters.$sort));
+		}
+
+		if (filters.$skip){
+      values = values.slice(parseInt(filters.$skip, 10));
+		}
+
+		if (filters.$limit){
+      values = values.slice(0, parseInt(filters.$limit, 10));
+		}
+
+    if(filters.$select) {
+      values = values.map(value => _.pick(value, filters.$select));
+    }
+
+    cb(null, values);
+  },
+
+  get(id, params, cb) {
+    if(typeof id === 'function') {
+      return id(new errors.BadRequest('An id needs to be provided'));
+    }
+
+    if (id in this.store) {
+      return cb(null, this.store[id]);
+    }
+
+    cb(new errors.NotFound(`No record found for id '${id}'`));
+  },
+
+  create(data, params, cb) {
+    var id = data[this._id] || this._uId++;
+    data = _.extend({}, data, { [this._id]: id });
+
+    if (this.store[id]){
+      return cb(new errors.Conflict('A record with id: ' + id + ' already exists'));
+    }
+
+    this.store[id] = data;
+
+    cb(null, data);
+  },
+
+  update(id, data, params, cb) {
+    if (id in this.store) {
+      data = _.extend({}, data, { [this._id]: id });
+      this.store[id] = data;
+
+      return cb(null, this.store[id]);
+    }
+
+    cb(new errors.NotFound(`No record found for id '${id}'`));
+  },
+
+  patch(id, data, params, cb) {
+    if (id in this.store) {
+      _.each(data, (value, key) => {
+        if(key !== this._id) {
+          this.store[id][key] = value;
+        }
+    });
+
+      return cb(null, this.store[id]);
+    }
+
+    cb(new errors.NotFound(`No record found for id '${id}'`));
+  },
+
+  remove(id, params, cb) {
+    if (id in this.store) {
+      const deleted = this.store[id];
+      delete this.store[id];
+
+      return cb(null, deleted);
+    }
+
+    cb(new errors.NotFound(`No record found for id '${id}'`));
+  }
+});
+
+module.exports = function(options) {
+  return Proto.create.call(MemoryService, options);
+};
+
+module.exports.Service = MemoryService;
