5c86acd28ccbe79f36846ffb3da59e70125da350
David Luecke <daff@neyeon.com>
2016-11-09 22:37:56 -0800

    Use feathers-commons utils

--- src/index.js
+++ src/index.js
@@ -1,33 +1,7 @@
 import Proto from 'uberproto';
 import filter from 'feathers-query-filters';
 import errors from 'feathers-errors';
-import { sorter, matcher } from 'feathers-commons/lib/utils';
-
-const _ = {
-  values (obj) {
-    return Object.keys(obj).map(key => obj[key]);
-  },
-  isEmpty (obj) {
-    return Object.keys(obj).length === 0;
-  },
-  extend (...args) {
-    return Object.assign(...args);
-  },
-  omit (obj, ...keys) {
-    const result = Object.assign({}, obj);
-    for (let key of keys) {
-      delete result[key];
-    }
-    return result;
-  },
-  pick (source, ...keys) {
-    const result = {};
-    for (let key of keys) {
-      result[key] = source[key];
-    }
-    return result;
-  }
-};
+import { sorter, matcher, _ } from 'feathers-commons';
 
 class Service {
   constructor (options = {}) {
